2345acb9cb27d25202f2966f0be2c5ed23758bc9
niceoneallround <niceoneallround@gmail.com>
2018-04-02 09:54:41 -0700

    Continue to build out

--- initnode.sh
+++ initnode.sh
@@ -3,10 +3,12 @@
 # Initialize the nodes chain data using the genesis file
 
 ENODE_NAME=$1
-CHAINDATA_ROOT=${CHAINDATA_ROOT:-"$(pwd)/ethereum-$ENODE_NAME"}
-CODE_ROOT=${CODE_ROOT:-"$(pwd)/ethereum-tools"}
+#CHAINDATA_ROOT=${CHAINDATA_ROOT:-"$(pwd)/ethereum-$ENODE_NAME"}
+#CODE_ROOT=${CODE_ROOT:-"$(pwd)/ethereum-tools"}
+CHAINDATA_ROOT="$(pwd)/$ENODE_NAME-data"
+CODE_ROOT="$(pwd)/ethereum-tools"
 
-echo "Enode Name is $ENODE_NAME"
+echo "Enode name is $ENODE_NAME"
 echo "Code root in $CODE_ROOT"
 echo "Chain Data in $CHAINDATA_ROOT"
 
@@ -18,4 +20,4 @@ fi
 docker run --rm \
   -v $CHAINDATA_ROOT:/root/.ethereum \
   -v $CODE_ROOT/files/genesis.json:/opt/genesis.json \
-  ethereum/client-go init /opt/genesis.json --datadir /root/.ethereumpwd
+  ethereum/client-go init /opt/genesis.json --datadir /root/.ethereum
