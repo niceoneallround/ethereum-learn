73c6498c5fcaf07ce704842a2d6004e6e052b43f
niceoneallround <niceoneallround@gmail.com>
2018-04-02 09:27:32 -0700

    fix typo

--- initnode.sh
+++ initnode.sh
@@ -7,7 +7,7 @@ DATA_ROOT=${CHAINDATA_ROOT:-"$(pwd)/data-$ENODE_NAME"}
 
 echo "Chain Data in $CHAINDATA_ROOT"
 
-docker run -rm \
+docker run --rm \
   -v $CHAINDATA_ROOT:/root/.ethereum \
   -v $(pwd)/files/genesis.json:/opt/genesis.json \
   ethereum/client-go init /opt/genesis.json --datadir /root/.ethereum -verbosity 5
